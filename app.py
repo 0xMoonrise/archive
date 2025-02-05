e05d76f684680d2a53624ba10423aa548a4d999e
0xMoonrise <josephxcharly@gmail.com>
2025-02-05 19:51:22 +0000

    Implement markdown rendering in view_md endpoint

--- app.py
+++ app.py
@@ -3,37 +3,59 @@ import os
 
 from flask import Flask, render_template,\
     send_from_directory, url_for, request, jsonify
-# from utils.utils import is_valid_file_ext
+import markdown
 
 app = Flask(__name__)
 
-PDF_FOLDER = os.path.join('lectures')
-app.config['UPLOAD_FOLDER'] = PDF_FOLDER
-# app.config["TEMPLATES_AUTO_RELOAD"] = True
-# app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+FOLDER = os.path.join('lectures')
+app.config['UPLOAD_FOLDER'] = FOLDER
 
-if not os.path.exists(PDF_FOLDER):
-    os.makedirs(PDF_FOLDER)
+if not os.path.exists(FOLDER):
+    os.makedirs(FOLDER)
 
 
 @app.route('/')
 def index():
-    extensions = ('.pdf', '.md')
-    pdf_files = [f for f in os.listdir(PDF_FOLDER) if f.endswith(extensions)]
-    return render_template('index.html', pdf_files=pdf_files)
+    pdf_files = [f for f in os.listdir(FOLDER) if f.endswith('.pdf')]
+    md_files = [f for f in os.listdir(FOLDER) if f.endswith('.md')]
+
+    return render_template('index.html',
+                           pdf_files=pdf_files,
+                           md_files=md_files)
 
 
-@app.route('/pdf/<filename>')
-def serve_pdf(filename):
-    return send_from_directory(PDF_FOLDER, filename)
+@app.route('/file/<filename>')
+def serve_file(filename):
+    return send_from_directory(FOLDER, filename)
 
 
-@app.route('/view/<filename>')
+@app.route('/view_pdf/<filename>')
 def view_pdf(filename):
-    page = request.args.get('page', default=1, type=int)
-    return render_template('view_pdf.html',
-                           pdf_url=url_for('serve_pdf',
-                                           filename=filename), page=page)
+    if filename.endswith('.pdf'):
+        page = request.args.get('page', default=1, type=int)
+        return render_template(
+            'view_pdf.html',
+            pdf_url=url_for(
+                'serve_file',
+                filename=filename),
+            page=page)
+    return "File not found", 404
+
+
+@app.route('/view_md/<filename>')
+def view_md(filename):
+    if filename.endswith('.md'):
+        with open(os.path.join(FOLDER, filename), "r", encoding="utf-8") as f:
+            md_content = f.read()
+
+        return render_template(
+            'view_md.html',
+            md_url=url_for('serve_file', filename=filename),
+            content=markdown.markdown(
+                md_content,
+                extensions=['fenced_code'],
+                output_format="html5"))
+    return "File not found", 404
 
 
 @app.route('/upload', methods=['POST'])
